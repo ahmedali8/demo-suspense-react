35d6d3f788093f0221b5fe8f7a9db6f6bb501399
ahmedali8 <imahmedalibhatti@gmail.com>
2020-08-14 20:33:27 +0500

    suspense article#1: added display fetched data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react';
+import createResource from './resource';
+import PostList from './PostList';
+
+const resource = createResource();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div >
+			<h1>Blog Posts</h1>
+			<Suspense fallback={<h1>Loading...</h1>}>
+				<PostList resource={resource} />
+			</Suspense>
+		</div>
+	);
 }
 
 export default App;
